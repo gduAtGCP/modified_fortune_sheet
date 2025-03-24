f1d231874b702566b7ec917bf660bf652229f70b
Eric Maddy <eric.maddy@gmx.com>
2025-03-24 18:23:26 -0400

    run dev but not build

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
-//import './App.css'
-//import React from 'react';
-//import ReactDOM from 'react-dom';
-import { Workbook } from "@fortune-sheet/react";
-import "@fortune-sheet/react/dist/index.css"
+import { Workbook } from "./FS/react";
+import "./FS/react/src/index.css"
 
 function App() {
  return  <Workbook data={[{ name: "Sheet1" }]} />;
